04bcb4d9220ff8b58089d026cd0913d90ddba614
Luis Miguel Marmolejo Peña <lucncd@gmail.com>
2020-02-25 18:50:01 -0500

    Se generan calculos de los datos y tablas de frecuencia, persiste error de filtrado en Nombre_Programa ya que aun muestra los valores eliminados de las filas

--- Ejercicio_clase_02.R
+++ Ejercicio_clase_02.R
@@ -36,11 +36,75 @@ table(pregrado)
 
 #Solo datos de promedio de ingenierias y de pregrado
 #Frecuencia Por Ingenieria, Tabla de contingencia 
+
 num_Ing<-grepl("INGENIERIA",graduados$NOMBRE_PROGRAMA)
 
 #which(datos$Nombre_Programa == "INGENIERIA", arr.ind = TRUE)
 #prog_ing<-str_match(datos$Nombre_Programa, "INGENIERIA")
+
 datos <-data.frame("Nombre_Facultad" = graduados$NOMBRE_FACULTAD[num_Ing], "Nombre_Programa" = graduados$NOMBRE_PROGRAMA[num_Ing],"Sexo" = graduados$SEXO[num_Ing], "Promedio" = graduados$PACUMULADO[num_Ing] / 10)
 num_Pos<-grepl("ESPECIALIZACION*|MAES*|DOCTORADO.*",datos$Nombre_Programa)
 datos_ing<-data.frame("Nombre_Facultad" = datos$Nombre_Facultad[!num_Pos], "Nombre_Programa" = datos$Nombre_Programa[!num_Pos],"Sexo" = datos$Sexo[!num_Pos], "Promedio" = datos$Promedio[!num_Pos])
+summary(datos_ing)
+summary(datos2)
+#Datos Proemdio de notas Ingenieria -------------------------------------------------------------------------------------
+#Media
+mean(datos_ing$Promedio)
+#Desviacion Estandar
+sd(datos_ing$Promedio)
+#Mediana 
+sort(datos_ing$Promedio)
+median(datos_ing$Promedio)
+#Frecuencia
+frecuencia_promedio<-table(datos_ing$Promedio)
+frecuencia_promedio
+#Moda
+moda_promedio<-sort(frecuencia_promedio, decreasing = TRUE)
+moda_promedio
+rownames(moda_promedio)[1];
+#Grafica
+barplot(moda_promedio, xlab = "Notas", ylab = "Frecuencia", main = "Frecuencia de notas Ingenierias")
+
+#Datos Sexo Ingenieria --------------------------------------------------------------------------------------------------
+#Media
+#mean(datos_ing$Sexo)
+#Desviacion Estandar
+#sd(datos_ing$Sexo)
+#Mediana 
+#sort(datos_ing$Sexo)
+#median(datos_ing$Sexo)
+#Frecuencia
+frecuencia_sexo<-table(datos_ing$Sexo)
+frecuencia_sexo
+#Moda
+moda_sexo<-sort(frecuencia_sexo, decreasing = TRUE)
+moda_sexo
+rownames(moda_sexo)[1];
+#Grafica
+barplot(moda_sexo, xlab = "Sexo", ylab = "Frecuencia", main = "Frecuencia de sexo en Ingenierias")
+
+
+#Datos Sexo Nombre Programa --------------------------------------------------------------------------------------------------
+#Media
+#mean(datos_ing$Nombre_Programa)
+#Desviacion Estandar
+#sd(datos_ing$Nombre_Programa)
+#Mediana 
+#sort(datos_ing$Nombre_Programa)
+#median(datos_ing$Nombre_Programa)
+#Frecuencia
+frecuencia_programa<-table(datos_ing$Nombre_Programa)
+frecuencia_programa
+#Moda
+moda_programa<-sort(frecuencia_programa, decreasing = TRUE)
+moda_programa
+rownames(moda_programa)[1];
+#Grafica
+barplot(moda_programa, xlab = "Programa", ylab = "Frecuencia", main = "Frecuencia de Ingenierias")
+
+#Tabla COntingencia ------------------------------------------------------------------------------------------------------
 table(datos_ing$Sexo,datos_ing$Promedio)
+table(datos_ing$Nombre_Programa)
+prop.table(datos_ing$Nombre_Programa)
+
+
