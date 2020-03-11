f12d1720ac20a9368bf0a7ebd20acebcff5ff7b2
luchoss10 <lucncd@gmail.com>
2020-03-10 22:20:21 -0500

    Generacion de tablas de frecuencia marginales proporcionales para todas las ingenierias

--- Taller1_Inferencia.R
+++ Taller1_Inferencia.R
@@ -1,52 +1,14 @@
 #Obtención de datos
 graduados<-read.csv("https://www.datos.gov.co/api/views/ha9e-e48s/rows.csv?accessType=DOWNLOAD")
-#Seleccion de columnas
-estrato<-graduados$ESTRATO
-promedio<-graduados$PACUMULADO
-programa<-graduados$NOMBRE_PROGRAMA
-facultad<-graduados$NOMBRE_FACULTAD
-#Estandarizacion de los datos para decimales en las notas del promedio
-promedio<-promedio/10
 
-#Muestra las primeras variables de las columnas 
-head(estrato)
-head(programa)
-head(promedio)
-
-#Caracteristicas de los datos
-summary(promedio)
-summary(estrato)
-#summary(programa) No se puede usar summary con variables categoricas solo con numericas 
-levels(programa)
-
-library(stringr)
-
-#Para instalar la libreria
-#install.packages("stringr")
-
-posgrados <- str_detect(programa,"MAES*|DOCTORADO*|ESPECIALIZACION.*")
-pregrado<-programa[!posgrados]
-notas_pregrado<-programa[!posgrados]
-length(pregrado)
-length(programa)
-length(notas_pregrado)
-table(pregrado)
-
-#datos <- data.frame(graduados$NOMBRE_FACULTAD, graduados$NOMBRE_PROGRAMA, graduados, programa)
-
-#Solo datos de promedio de ingenierias y de pregrado
-#Frecuencia Por Ingenieria, Tabla de contingencia 
-
+#Filtrado de datos por pregrado de ingenieria.
 num_Ing<-grepl("INGENIERIA",graduados$NOMBRE_PROGRAMA)
-
-#which(datos$Nombre_Programa == "INGENIERIA", arr.ind = TRUE)
-#prog_ing<-str_match(datos$Nombre_Programa, "INGENIERIA")
-
 datos <-data.frame("Nombre_Facultad" = graduados$NOMBRE_FACULTAD[num_Ing], "Nombre_Programa" = graduados$NOMBRE_PROGRAMA[num_Ing],"Sexo" = graduados$SEXO[num_Ing], "Promedio" = graduados$PACUMULADO[num_Ing] / 10)
 num_Pos<-grepl("ESPECIALIZACION*|MAES*|DOCTORADO.*",datos$Nombre_Programa)
 datos_ing<-data.frame("Nombre_Facultad" = datos$Nombre_Facultad[!num_Pos], "Nombre_Programa" = datos$Nombre_Programa[!num_Pos],"Sexo" = datos$Sexo[!num_Pos], "Promedio" = datos$Promedio[!num_Pos])
+
 summary(datos_ing)
-summary(datos2)
+
 #Datos Proemdio de notas Ingenieria -------------------------------------------------------------------------------------
 #Media
 mean(datos_ing$Promedio)
@@ -89,12 +51,15 @@ rownames(moda_programa)[1];
 barplot(moda_programa, xlab = "Programa", ylab = "Frecuencia", main = "Frecuencia de Ingenierias")
 
 #Tabla COntingencia ------------------------------------------------------------------------------------------------------
-sex_prom<-table(datos_ing$Sexo,datos_ing$Promedio)
+sex_prom<-table(datos_ing$Promedio,datos_ing$Sexo)
+sex_prom
 prog_prom<-table(datos_ing$Nombre_Programa, datos_ing$Promedio)
 sex_nombre<-table(datos_ing$Sexo, datos_ing$Nombre_Programa)
+#Tabla de frecuencia relativas marginales
 prop.table(sex_prom)
-
-
-datos_ing$Rendimiento[datos_ing$Promedio<3.5] <- "bajo"
-datos_ing$Rendimiento[datos_ing$Promedio>=3.5 & datos_ing$Promedio<=4.0] <- "medio"
-datos_ing$Rendimiento[datos_ing$Promedio>4.0] <- "alto"
+#Para Filas
+prop.table(sex_prom,1)
+rowSums(prop.table(sex_prom,1))
+#Para Columnas
+prop.table(sex_prom,2)
+colSums(prop.table(sex_prom,2))
