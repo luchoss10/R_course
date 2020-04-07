c3778f43cae2e17aa3bfabaca19c3221075fdf88
Luis Miguel Marmolejo Peña <lucncd@gmail.com>
2020-04-07 18:57:46 -0500

    Finalizacion Ejercicio clase muestras de vehiculos

--- Teoria_grandes_numero_muestras.R
+++ Teoria_grandes_numero_muestras.R
@@ -50,10 +50,43 @@ ds_xb<-ds/sqrt(n);ds_xb
 z<-(xb-miu)/ds_xb;z
 
 pnorm(z)
+
 1- pnorm(z)
 (1- pnorm(z))*100
 
-
+#Sample
+sample(1:10,4,replace = T)
+sample(50:100,10,replace = F)
+runif(5, min = 4, max = 5)
+runif(5,3,4)
 
 vehiculos<-read.csv("https://www.datos.gov.co/api/views/x9pp-pcn5/rows.csv?accessType=DOWNLOAD")
-#Sample
+auto_moviles<-subset(vehiculos, vehiculos$CLASE == "AUTOMOVIL")
+muestra1<-auto_moviles[sample(nrow(auto_moviles), 1500, replace = F),1:6]
+muestra2<-auto_moviles[sample(nrow(auto_moviles), 1500, replace = F),1:6]
+
+#---------------Población--------------
+n=1500
+ux_P<-mean(auto_moviles$MODELO);ux
+sd_P=sd(auto_moviles$MODELO);sd
+
+#----------------Muestra 1---------------
+
+x1<-mean(muestra1$MODELO);ux
+S1=sd_P/sqrt(n);sd
+z1<-(2017-ux_P)/S1;z1
+z1_1<-(2014-ux_P)/S1;z1_1
+(1-pnorm(z1))*100
+
+(pnorm(z1)-pnorm(z1_1))*100
+
+#----------------Muestra 2---------------
+
+x2<-mean(muestra2$MODELO);ux
+S2=sd_P/sqrt(n);sd
+z2<-(2017-ux_P)/S2;z2
+z2_2<-(2014-ux_P)/S2;z2_2
+(1-pnorm(z2))*100
+
+(pnorm(z2)-pnorm(z2_2))*100
+
